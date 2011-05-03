a086d31067d0f0ec3cbfe79a5fea4f7e2721e220
mwichary@google.com <mwichary@google.com@72a760f7-aed3-7fd5-e778-9edbf0160847>
2011-05-03 17:46:46 +0000

    --

--- trunk/slides.js
+++ trunk/slides.js
@@ -508,7 +508,7 @@ function addGeneralStyle() {
   
   var el = document.createElement('meta');
   el.name = 'viewport';
-  el.content = 'height=800';
+  el.content = 'width=1100,height=750';
   document.querySelector('head').appendChild(el);
   
   var el = document.createElement('meta');
